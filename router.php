741bc1a418628ba8200c6ee75240ebedb9bfa3b5
Gerti <manajgerti2002@gmail.com>
2026-03-17 18:41:11 +0100

    fix: aggiungi router.php per Railway

---
+++ router.php
@@ -0,0 +1,15 @@
+<?php
+$path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$file = __DIR__ . $path;
+
+if ($path !== '/' && file_exists($file) && !is_dir($file)) {
+    return false;
+}
+
+$index = __DIR__ . '/index.html';
+if (file_exists($index)) {
+    include $index;
+} else {
+    http_response_code(404);
+    echo '404 Not Found';
+}
